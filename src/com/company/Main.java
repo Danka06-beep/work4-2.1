f8e650082512965eb17650a06e973c4003641b7f
Danka06 <kamikoto02@mail.ru>
2021-07-04 22:29:34 +0300

    аа

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,45 +1,50 @@
 package com.company;
 
-import java.util.InputMismatchException;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-public static final int StartFloor = 0;
-public static final int EndFloor = 25;
+    public static final int StartFloor = 0;
+    public static final int EndFloor = 25;
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Queue<Integer> qu = new PriorityQueue();
-        System.out.println("Привет, на данный момент вы находитесь на 0-ом этаже.");
+        Scanner scanner = new Scanner(System.in);
+
+        Deque<Integer> floors = new ArrayDeque<>();
+        floors.add(0);
+
+        System.out.printf("Привет, вы начинаете с: %d этажа\n", floors.getFirst());
+        int waitDoorsInSeconds = 10;
+        int waitMoveInSeconds = 5;
 
         while (true) {
-            System.out.println("Введите номер этажа от 0 до 25 (Для завершения введите 0)");
-            int floor;
-         try{
-             floor = sc.nextInt();
-             if (floor < StartFloor || floor > EndFloor) {
-                 System.err.println("Вы ввели  неправильный этаж, попробуйте снова");
-                 continue;
-             }
-             qu.add(floor);
-             if(floor == 0){
-                 break;
-             }
-         }catch (InputMismatchException e) {
-             System.err.println("Ошибка! Введите номер этажа");
-             sc.nextLine();
+            System.out.println("Введите номер этажа (для завершения введите 0)");
+            String input = scanner.nextLine();
+            int x = Integer.parseInt(input);
+            if (x > EndFloor || x < StartFloor) {
+                System.err.println("Такого этажа нет!");
+            } else if (x == 0) {
+                System.out.println("Лифт поехал:");
+                floors.poll();
+                System.out.println("Этажи: " + floors);
+
+                int previousFloor = -1;
+                int totalSeconds = 0;
+                while (floors.size() > 0) {
+                    int currentFloor = floors.remove();
+                    if (previousFloor != -1) {
+                        totalSeconds += Math.abs(currentFloor - previousFloor) * waitMoveInSeconds;
+                        totalSeconds += waitDoorsInSeconds;
+                    }
+                    previousFloor = currentFloor;
+                }
+                System.out.println("Время затраченное лифтом на маршрут: " + totalSeconds + " с.");
+                break;
+            } else {
+                floors.add(x);
             }
         }
-        System.out.println();
-        System.out.println("Лифт поехал!");
+    }
+}
 
-        while(!qu.isEmpty()){
-            Integer floor = qu.poll();
-            System.out.println("Этаж:"  + floor);
 
-        }
-        }
-        }
 
